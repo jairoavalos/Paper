699f516a5a1c82df1da309eeda788f1a1989139e
Jairo Avalos <jairo@palateapp.com>
2014-06-19 00:05:50 -0700

    Initial Commit

---
+++ Paper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paper
+//
+//  Created by Jairo Avalos on 6/19/14.
+//  Copyright (c) 2014 Jairo Avalos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
